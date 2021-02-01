17e495d1f1bb74a06cc44611782b050cd0f08d56
Yasuo Ashina <ashipp3000@gmail.com>
2021-02-01 10:02:26 +0900

    new project

---
+++ GitUpGraph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitUpGraph
+//
+//  Created by aship on 2021/01/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
